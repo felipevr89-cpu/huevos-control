2051c04f6f97c0e44fe1d39695998881771c3d5c
Felipe <felipe@example.com>
2026-07-21 07:47:02 -0400

    Fix PWA: manifest id, maskable icons, apple-touch-icon, service worker relativo

--- sw.js
+++ sw.js
@@ -1,22 +1,47 @@
-const CACHE = 'huevos-v1'
-const FILES = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.json', '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png']
+const CACHE = 'huevos-v2'
+const FILES = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-192.png',
+  './icons/icon-maskable-512.png',
+  './icons/apple-touch-icon.png'
+]
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)))
-  self.skipWaiting()
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(FILES))
+      .then(() => self.skipWaiting())
+  )
 })
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))))
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  )
 })
 
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      const clone = res.clone()
-      caches.open(CACHE).then(c => c.put(e.request, clone))
-      return res
-    }))
+    caches.match(e.request).then(r => {
+      if (r) return r
+      return fetch(e.request).then(res => {
+        const clone = res.clone()
+        caches.open(CACHE).then(c => c.put(e.request, clone))
+        return res
+      }).catch(() => {
+        if (e.request.destination === 'document') {
+          return caches.match('./index.html')
+        }
+      })
+    })
   )
 })
