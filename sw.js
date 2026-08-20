e5878dfd1c207ef1164bcde11a71eb25c37f9505
Felipe <felipe@huevos.app>
2026-08-20 16:02:54 -0400

    fix: bump sw cache v7 - forzar actualizacion de la app en dispositivos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'huevos-v6'
+const CACHE = 'huevos-v7'
 const STATIC_FILES = ['/', '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png']
 const DYNAMIC_FILES = ['/index.html', '/styles.css', '/app.js', '/manifest.json']
 
