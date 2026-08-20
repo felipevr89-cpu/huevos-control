56b8e241b87ba7c7d8eff5f48ef16cf20254715f
Felipe <felipe@huevos.app>
2026-08-20 10:08:40 -0400

    fix: rewrite all files - edit orders, future deliveries, update detection

--- sw.js
+++ sw.js
@@ -1,22 +1,48 @@
-const CACHE = 'huevos-v1'
-const FILES = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.json', '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png']
+const CACHE = 'huevos-v4'
+const STATIC_FILES = ['/', '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png']
+const DYNAMIC_FILES = ['/index.html', '/styles.css', '/app.js', '/manifest.json']
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)))
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([...STATIC_FILES, ...DYNAMIC_FILES]))
+  )
   self.skipWaiting()
 })
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))))
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('message', e => {
+  if (e.data && e.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
+  }
 })
 
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      const clone = res.clone()
-      caches.open(CACHE).then(c => c.put(e.request, clone))
-      return res
-    }))
-  )
+  const url = new URL(e.request.url)
+  const isDynamic = DYNAMIC_FILES.some(f => url.pathname === f)
+  if (isDynamic) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone()
+        caches.open(CACHE).then(c => c.put(e.request, clone))
+        return res
+      }).catch(() => caches.match(e.request))
+    )
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+        const clone = res.clone()
+        caches.open(CACHE).then(c => c.put(e.request, clone))
+        return res
+      }))
+    )
+  }
 })
